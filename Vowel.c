bb877c3e64e34d220962eb06c925e28c5aac44ed
PriyadharshniShanmugavel <priyadharshni6936@gmail.com>
2024-10-22 21:00:47 +0530

    Create Vowel.c

---
+++ Vowel.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+void main()
+
+{
+
+char ch;
+
+clrscr();
+
+printf(“\n Enter a character:”);
+
+scanf(“%c”,&ch);
+
+if(ch==’A’ || ch==’E’ || ch==’I’ || ch==’O’ || ch==’U’ || ch==’a’ || ch==’e’ || ch==’i’ || 
+
+ ch==’o’ || ch==’u’)
+
+{
+
+printf(“\n The given character is a vowel”);
+
+}
+
+else
+
+{
+
+ printf(“\n The given character is a consonant”);
+}
+
+ getch();
+}
+<!---
+PriyadharshniShanmugavel/PriyadharshniShanmugavel is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
